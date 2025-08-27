0688c3277a3cf0d94405c90d3e2ee6f21a710476
ntdhk67 <daehana06@kakao.com>
2025-08-28 08:28:25 +0900

    Add files via upload

---
+++ KG_2/KG_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace KG_2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            GameCore core = new GameCore();
+
+            core.Run();
+
+
+        }
+    }
+}
